737a85673738720fae6f6ea36874bf16a430c602
Pili Sanchez <sanchezpili6@MacBook-Pro.local>
2025-09-03 22:49:13 -0600

    Add ugly nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import LocationCard from './components/Card'
+import MainNavBar from './components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <MainNavBar></MainNavBar>
       <LocationCard
         locationName="Casa"
         locationImage="https://i.pinimg.com/1200x/24/2d/e0/242de0a8581dd00744edbe5d9b118a9d.jpg"
